b28cb50dbd1323f17830f74bf5be9155129e6fb9
Lovelyboy-Sri <sridhar1261@gmail.com>
2026-05-02 12:58:53 +0530

    Permissions are handled properly

--- server/src/index.ts
+++ server/src/index.ts
@@ -39,9 +39,10 @@ app.use(cors({
         const allowedOrigins = [
             'http://localhost:5173',
             'http://127.0.0.1:5173',
-            'http://localhost:3000'
+            'http://localhost:3000',
+            'https://ozofi-homie.vercel.app'
         ];
-        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        if (!origin || allowedOrigins.indexOf(origin) !== -1 || origin.endsWith('.vercel.app')) {
             callback(null, true);
         } else {
             callback(new Error('Not allowed by CORS'));
